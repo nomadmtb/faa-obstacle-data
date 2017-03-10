cced840efa03653e79e40e9b77f9a0d9f3ef5638
Kyle Luce <nomadmtb@gmail.com>
2017-03-09 21:59:05 -0600

    WIP
    WIP

--- faaobstacles/dof/management/commands/populatedof.py
+++ faaobstacles/dof/management/commands/populatedof.py
@@ -25,8 +25,27 @@ class Command(BaseCommand):
         for raw_line in dof_file:
             self.stdout.write(raw_line)
 
-            id = int(raw_line[3:8])
-            country = YOU ARE HERE
+            id = int((raw_line[3:9]).rstrip())
+            country = (raw_line[12:14]).rstrip()
+            state = (raw_line[15:17]).rstrip()
+            city = (raw_line[18:35]).rstrip()
+
+            lat_deg = int((raw_line[35:37]).rstrip())
+            lat_min = int((raw_line[38:40]).rstrip())
+            lat_sec = float((raw_line[41:46]).rstrip())
+
+            long_deg = int((raw_line[48:51]).rstrip())
+            long_min = int((raw_line[52:54]).rstrip())
+            long_sec = float((raw_line[55:60]).rstrip())
+
+            type_desc = (raw_line[62:74]).rstrip())
+            quantity = int(raw_line[75])
+            agl_height = int((raw_line[77:82]).rstrip())
+            amsl_height = int((raw_line[83:88]).rstrip())
+            lighting = (raw_line[89]).rstrip()
+
+            horizontal_accuracy = YOU ARE HERE
+
 
         dof_file.close()
 

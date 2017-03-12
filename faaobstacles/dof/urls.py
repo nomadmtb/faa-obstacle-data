a27e0c2799e1ab42766b697c6556d4e14e53fe26
Kyle Luce <nomadmtb@gmail.com>
2017-03-11 18:53:32 -0600

    WIP
    WIP

--- faaobstacles/dof/urls.py
+++ faaobstacles/dof/urls.py
@@ -1,4 +1,4 @@
-"""faaobstacles URL Configuration
+"""dof URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+from .views import CalculateRouteView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url('^dof/', include('dof.urls')),
+    url(
+        r'^api/calculate_route$',
+        CalculateRouteView.as_view(),
+        name='CalculateRouteView'
+    ),
 ]
--- faaobstacles/dof/views.py
+++ faaobstacles/dof/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Airport
+
 
 # Create your views here.
+class CalculateRouteView(View):
+
+    def get(self, request):
+        raw_path = request.GET.get('route', None)
+
+        if raw_path:
+            airports = self.__get_airport_route(raw_path)
+            print(airports)
+        else:
+            pass
+
+        return HttpResponse('result')
+
+    # Get the airport objects with the parsed airport codes from the HTTP Get
+    # params. Build dictionary of results in case some don't resolve.
+    def __get_airport_route(self, raw_route):
+
+        results = {
+            'airports': [],
+            'malformed': [],
+        }
+
+        for code in raw_route.split(','):
+            code = code.upper()
+
+            if code.isalpha() and len(code) < 5:
+                new_airport = None
+
+                try:
+                    new_airport = Airport.objects.get(iata=code)
+                except Airport.DoesNotExist:
+                    pass
+
+                if not new_airport:
+                    try:
+                        new_airport = Airport.objects.get(icao=code)
+                    except Airport.DoesNotExist:
+                        pass
+
+                if new_airport:
+                    results['airports'].append(new_airport)
+
+                else:
+                    results['malformed'].append(code)
+            else:
+                pass
+
+        return results
